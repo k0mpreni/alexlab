6cf7ad29ddf45c2c3391a6b8512c30c2d6254714
Alex Labonne <>
2019-11-06 14:32:39 +0100

    test fluid transition

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { send, receive } from "../crossfade";
+</script>
+
 <style>
   .wrapper {
     line-height: 2rem;
@@ -8,31 +12,35 @@
   <title>Alex Lab</title>
 </svelte:head>
 
-<div class="wrapper">
-  <p>🇫🇷 French and german 🇩🇪 developer here ! 🤖</p>
-  <p>
-    🎓I did a Higher National Diploma in International Business in sandwich
-    courses.
-  </p>
-  <p>
-    💡I could develop competences like customer relationship, project management
-    and team bulding
-  </p>
-  <p>
-    💻 I wanted to increase my competences so I followed an intensive training
-    to be a fullstack javascript web developer.
-  </p>
-  <p>
-    🎓 I challenged myself to learn to develop and I'm now diplomed of a
-    Associate Degree in Software Development.
-  </p>
-  <p>
-    ⌨️ Now I apply those competences in an agile work environment based on open
-    communication, kindness, learning to perform.
-  </p>
-  <p>
-    🗝 I currently learn a lot by myself about computer science and related
-    topics especially cybersecurity.
-  </p>
+<main>
+  <h1 out:send="{{key: 'title'}}" in:receive="{{key: 'title'}}">Test</h1>
 
-</div>
+  <div class="wrapper">
+    <p>🇫🇷 French and german 🇩🇪 developer here ! 🤖</p>
+    <p>
+      🎓I did a Higher National Diploma in International Business in sandwich
+      courses.
+    </p>
+    <p>
+      💡I could develop competences like customer relationship, project
+      management and team bulding
+    </p>
+    <p>
+      💻 I wanted to increase my competences so I followed an intensive training
+      to be a fullstack javascript web developer.
+    </p>
+    <p>
+      🎓 I challenged myself to learn to develop and I'm now diplomed of a
+      Associate Degree in Software Development.
+    </p>
+    <p>
+      ⌨️ Now I apply those competences in an agile work environment based on
+      open communication, kindness, learning to perform.
+    </p>
+    <p>
+      🗝 I currently learn a lot by myself about computer science and related
+      topics especially cybersecurity.
+    </p>
+
+  </div>
+</main>
--- src/routes/random.svelte
+++ src/routes/random.svelte
@@ -1,14 +1,19 @@
+<script>
+  import { send, receive } from "../crossfade";
+</script>
+
 <style>
   ul {
     margin: 0 0 1em 0;
     line-height: 1.5;
   }
 </style>
+<main>
+  <h1 out:send="{{key: 'title'}}" in:receive="{{key: 'title'}}">Random stuff</h1>
 
-<h1>Random stuff</h1>
-
-<ul>
-  <li>
-    <a href="readings">My 2019 readings</a>
-  </li>
-</ul>
+  <ul>
+    <li>
+      <a href="readings">My 2019 readings</a>
+    </li>
+  </ul>
+</main>
