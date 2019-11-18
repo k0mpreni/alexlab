abd066208d8363da7f3f88dcaa80ef694b3bd40b
Alex Labonne <>
2019-11-18 21:06:21 +0100

    add sec headers

--- src/server.js
+++ src/server.js
@@ -2,16 +2,66 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import uuidv4 from 'uuid/v4';
+import helmet from 'helmet';
+import expectCt from 'expect-ct';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use((req, res, next) => {
+    res.locals.nonce = uuidv4();
+    next();
+  })
+  .use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
+          blockAllMixedContent: true
+        }
+      }
+    })
+  )
+  .use(helmet.xssFilter())
+  .use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }))
+  .use(
+    helmet.featurePolicy({
+      features: {
+        camera: ["'none'"],
+        microphone: ["'none'"],
+        speaker: ["'self'"],
+        vibrate: ["'none'"],
+        geolocation: ["'none'"],
+        accelerometer: ["'none'"],
+        ambientLightSensor: ["'none'"],
+        autoplay: ["'none'"],
+        encryptedMedia: ["'none'"],
+        gyroscope: ["'none'"],
+        magnetometer: ["'none'"],
+        midi: ["'none'"],
+        payment: ["'none'"],
+        pictureInPicture: ["'none'"],
+        usb: ["'none'"],
+        vr: ["'none'"],
+        fullscreen: ['*']
+      }
+    })
+  )
+  .use(helmet.hidePoweredBy())
+  .use(helmet.noSniff())
+  .use(
+    helmet.frameguard({
+      action: 'deny'
+    })
+  )
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
